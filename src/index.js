1dfee4d916182c5438a1197f994fd886838904ff
Stephanie912-lab <stephanieamondi@gmail.com>
2024-10-15 18:53:32 +0300

    added axios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
+function refreshWeather(response){
+
+}
+
+
+
+
+
+
+
+
+function searchCity(city){
+    let apiKey="4b848ft03of375a4da055d8efff97074"
+    let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${"city"}&key=${"apiKey"}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput=document.querySelector("#search-form-input");
     let cityElement=document.querySelector("#city");
     cityElement.innerHTML=searchInput.value;
+    searchCity(searchInput.value);
 }
 
 
