b94a91383f7ae632028a828e4d589b00bdd23aee
Stephanie912-lab <stephanieamondi@gmail.com>
2024-10-16 20:16:25 +0300

    application of real data using the API

--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
 function refreshWeather(response){
     let temperatureElement=document.querySelector("#temperature");
     let cityElement=document.querySelector("#city");
+    let descriptionElement=document.querySelector("#description");
+    let humidityElement=document.querySelector("#humidity");
+    let windSpeedElement=document.querySelector("#wind-speed");
+    let timeElement=document.querySelector("#time");
+    let date=new Date(response.data.time * 1000);
+
+    
 
 
     cityElement.innerHTML= response.data.city;
-
+    timeElement.innerHTML=formatDate(date);
+    descriptionElement.innerHTML=`,${response.data.condition.description}`;
+    humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML=response.data.temperature.current;
 
 
 }
 
+function formatDate(date){
+    let hours=date.getHours();
+    let minutes=date.getMinutes();
+    let days=["Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"];
 
+        let day=days[date.getDay()];
+        
+        if (minutes < 10){
+            minutes=`0${minutes}`;
+        }
+        return`${day} ${hours}:${minutes}`;
+}
 function searchCity(city){
     let apiKey="4b848ft03of375a4da055d8efff97074";
     let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
