e2f54474c69261cfe2a83f135da1740b59926008
Stephanie912-lab <stephanieamondi@gmail.com>
2024-10-17 21:01:11 +0300

    Added the icon element through javascript

--- src/index.js
+++ src/index.js
@@ -6,16 +6,17 @@ function refreshWeather(response){
     let windSpeedElement=document.querySelector("#wind-speed");
     let timeElement=document.querySelector("#time");
     let date=new Date(response.data.time * 1000);
-
+    let iconElement=document.querySelector("#icon");
     
 
-
+    iconElement.innerHTML=`<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
     cityElement.innerHTML= response.data.city;
     timeElement.innerHTML=formatDate(date);
     descriptionElement.innerHTML=`,${response.data.condition.description}`;
     humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
-    temperatureElement.innerHTML=response.data.temperature.current;
+    temperatureElement.innerHTML=Math.round (response.data.temperature.current);
+    
 
 
 }
